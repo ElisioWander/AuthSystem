27f4af276560ab1210d0486e1a5278ab92d48a48
elisioWander <elisio741@hotmail.com>
2022-07-07 17:04:09 -0300

    mobile-button-signout-and-home-image-bug-solved

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { useAuth } from "../../context/AuthContext";
+import { FaSignOutAlt } from "react-icons/fa"
 
 export function Header() {
   const { signOut } = useAuth()
@@ -137,43 +138,11 @@ export function Header() {
           </div>
           <div className="-mr-2 flex md:hidden">
             {/* <!-- Mobile menu button --> */}
-            <button
-              type="button"
-              className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-              aria-controls="mobile-menu"
-              aria-expanded="false"
+            <button 
+              className="text-zinc-200"
+              onClick={signOut}
             >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="block h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M4 6h16M4 12h16M4 18h16"
-                />
-              </svg>
-              <svg
-                className="hidden h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
+              <FaSignOutAlt className="mr-2" fontSize={24} />
             </button>
           </div>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,14 +32,18 @@ export default function Home() {
   }
 
   return (
-    <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-full flex items-center justify-center py-24 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
-          <Image 
-            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-            alt="Workflow"
-            className="mx-auto h-12 w-auto"
-          />
+          <div className="w-full flex flex-col items-center justify-center">
+            <Image 
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt="Workflow"
+              width={50}
+              height={48}
+            />
+          </div>
+
           <h2 className="mt-6 text-center text-3xl font-extrabold text-zinc-200">
             Sign in to your account
           </h2>
